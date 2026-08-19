96ff80451e7c24cca8096a045b148910535dcb93
perroflow28j-jpg <perroflow28j@gmail.com>
2026-08-19 10:39:18 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,31 @@
-const CACHE='chapolo-extremo-v23';
-const ASSETS=['./','./index.html','./src/app.js','./src/styles.css','./manifest.webmanifest','./chapolo-logo.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = 'chapolo-extremo-v23';
+const ASSETS = [
+  './',
+  './index.html',
+  './src/app.js',
+  './src/styles.css',
+  './manifest.webmanifest',
+  './chapolo-logo.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
